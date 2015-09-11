183affe0520714e97585b50911ebacc791371d6f
Gary Ashby <GaryAshby@outlook.com>
2015-09-11 14:48:20 +0200

    Fixes to Graph
    Updates Graph 3 a& 4

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ data <- cbind(data, weekday)
 with(data, plot(datetime, Sub_metering_1, type = 'l' , xlab = '', ylab = 'Energy sub metering'))
 with(data, lines(datetime, Sub_metering_2, type = 'l', col = 'red'))
 with(data, lines(datetime, Sub_metering_3, type = 'l', col = 'blue'))
-legend('topright', col = c('black','red','blue'), legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), pch = '-', lwd = 1)
+legend('topright', col = c('black','red','blue'), legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), pch =  NA, lwd = 1)
 
 #close device
 dev.off()    
--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ with(data, plot(datetime, Voltage, type = 'l', ylab = 'Voltage'))
 with(data, plot(datetime, Sub_metering_1, type = 'l' , xlab = '', ylab = 'Energy sub metering'))
 with(data, lines(datetime, Sub_metering_2, type = 'l', col = 'red'))
 with(data, lines(datetime, Sub_metering_3, type = 'l', col = 'blue'))
-legend('topright', col = c('black','red','blue'), legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), pch = '-', lwd = 1)
+legend('topright', col = c('black','red','blue'), legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), pch = NA, lwd = 1)
 #Plot 4
 with(data, plot(datetime, Global_reactive_power, type = 'l', ylab = 'Global_reactive_power'))
 
